ac5bf0991cb1641e1331840a7cf991e3f1643b33
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2019-11-10 23:42:46 +0900

    暗闇とライトの位置を調整するロジックを色々直した

--- src/index.js
+++ src/index.js
@@ -8,8 +8,15 @@ $(()=> {
         turnOn();
         updateLightPosition(event);
     })
+    $("body").on("touchstart",(event)=>{
+        turnOn();
+        updateLightPosition(event);
+    })
 
     $("body").on("mouseup",() =>{
+        turnOff();
+    })
+    $("body").on("touchend",()=>{
         turnOff();
     })
 
@@ -20,39 +27,63 @@ $(()=> {
     $("body").on("mousemove",(event) =>{
         updateLightPosition(event);
     })
+    $("body").on("touchmove",(event) =>{
+        updateLightPosition(event);
+    })
 
     
 })
 
 const turnOn = () => {
     $("#light").addClass("on");
+    $("#light-space").addClass("on");
     isTurnOn = true;
 }
 
 const turnOff = () => {
     $("#light").removeClass("on");
+    $("#light-space").removeClass("on");
     isTurnOn = false;
 }
 
 const updateLightPosition = (event) => {
+    if(!event.touches && !isPCMode()){
+        return;
+    }
+    
     if(!isTurnOn){
         return;
     }
 
-    const cursor = {
-        x: event.clientX,
-        y: event.clientY
-    }
     const light = {
-        width: $("#light").width(),
-        height: $("#light").height()
+        width: $("#light-space").width(),
+        height: $("#light-space").height()
     }
 
-    light.x = cursor.x - light.width/2
-    light.y = cursor.y - light.height/2
+    const position = isPCMode() ? event : event.touches[0];
+    const cursor = {
+        x:position.clientX
+        ,y: position.clientY
+    }
+    
+    light.x = Math.round(cursor.x - $(".light-wrapper").width()/2);
+    light.y = Math.round(cursor.y - $(".light-wrapper").height()/2);
 
-    console.log(light);
+    $(".light-wrapper").css("left",light.x);
+    $(".light-wrapper").css("top",light.y);
+    
+    $("#light").css("width",light.width);
+    $("#light").css("height",light.height);
+    $("#light").css("top",cursor.y - light.height/2);
+    $("#light").css("left",cursor.x - light.width/2);
+}
+const isPCMode = () => {
+    const ua = navigator.userAgent
+    if ((ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0) && ua.indexOf('Mobile') > 0) {
+        return false;
+    } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+        return false;
+    }
 
-    $("#wrapper-base-height-box").css("height",light.y);
-    $("#wrapper-base-width-box").css("width",light.x);
+    return true;
 }
